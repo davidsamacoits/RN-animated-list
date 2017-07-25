ddb2ad1da74ce58e4842a77410c89605111c959f
David Samacoits <david.samacoits@gmail.com>
2017-07-24 20:45:44 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {
+  View, Text
+} from 'react-native';
+
+class App extends Component {
+  // Render
+  render() {
+    return (
+      <View>
+        <Text>Hello</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
